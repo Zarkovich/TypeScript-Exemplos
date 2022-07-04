7b9407755be85be88dd774835779bcfed7393b79
Gustavo Vieira <gustavovieirazu@gmail.com>
2022-07-03 23:32:19 -0300

    .

---
+++ scripts/desafio1.js
@@ -0,0 +1,8 @@
+"use strict";
+// Como podemos rodar isso em um arquivo .ts sem causar erros?
+let employee = {
+    code: 10,
+    name: "james",
+};
+employee.code = 10;
+employee.name = "John";
---
+++ src/desafio1.ts
@@ -0,0 +1,8 @@
+// Como podemos rodar isso em um arquivo .ts sem causar erros?
+
+let employee: { code: number; name: string } = {
+    code: 10,
+    name: "james",
+};
+employee.code = 10;
+employee.name = "John";
